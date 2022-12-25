83aebcda13a5e5e6091f1c94c64c55836fabcd73
Malachi Soord <inverse.chi@gmail.com>
2022-12-25 22:08:02 +0100

    Redo

--- tools/image-optimise/script.sh
+++ tools/image-optimise/script.sh
@@ -2,7 +2,7 @@
 
 SEARCH_DIR=${1:-.}
 
-find ${SEARCH_DIR} -iname '*.jpeg' -o -iname '*.jpg' |  xargs jpegoptim --max=90 --strip-all --preserve --totals -v
+find ${SEARCH_DIR} -iname '*.jpeg' -o -iname '*.jpg' |  xargs jpegoptim --strip-all --preserve --totals -v
 
 
 find ${SEARCH_DIR} -iname '*.png' |  xargs optipng
